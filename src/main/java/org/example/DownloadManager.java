7c7db11fa6763630ad34dca5f6bd8a232acf9c29
TanasiienkoAnastasia <tanasiienkoanastasia@gmail.com>
2023-12-08 14:09:21 +0200

    download button clicked

--- src/main/java/org/example/DownloadManager.java
+++ src/main/java/org/example/DownloadManager.java
@@ -3,15 +3,21 @@ package org.example;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import org.example.models.FileInfo;
 
 public class DownloadManager {
 
     @FXML
     private TextField urlTextField;
 
+    public int index = 0;
     @FXML
     void downloadButtonClicked(ActionEvent event) {
-        String text = this.urlTextField.getText();
-        System.out.println("Field text is = " + text);
+
+        String url = urlTextField.getText().trim();
+        String filename = url.substring(url.lastIndexOf("/")+1);
+        String status = "STARTING";
+        String action = "OPEN";
+        FileInfo file = new FileInfo((index+1), filename, url, status, action);
     }
 }
